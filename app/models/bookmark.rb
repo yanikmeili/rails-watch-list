9208a45e0971c7840e1ed90e775aefcaafd375c8
Yanik Meili <yanikm41@gmail.com>
2021-06-03 11:57:37 +0200

    data base and model done

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :list_id, uniqueness: { scope: :movie_id }
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+end
