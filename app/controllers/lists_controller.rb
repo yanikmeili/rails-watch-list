97c5d3e8830d93afa9f6e59e03d6cef5c8ac5a02
Yanik Meili <yanikm41@gmail.com>
2021-06-04 13:00:51 +0200

    working on it

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,6 @@ class ListsController < ApplicationController
   end
   private
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
