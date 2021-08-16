23641c3b1fb07b3cab05e9f605d398a408d437fd
Yanik Meili <yanikm41@gmail.com>
2021-08-16 12:16:27 +0200

    changed the style

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-
   validates :list_id, uniqueness: { scope: :movie_id }
   validates :comment, presence: true
   validates :comment, length: { minimum: 6 }
